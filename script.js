4bb19f0c3ac5bafc170d51a557c07a4d2f0bf0e4
PrinceSharma16 <psprincesharma51@gmail.com>
2024-12-17 00:30:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
 const apikey="6979082e4d6a53269818bb2be110ffb5 ";
 const apiurl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox= document.querySelector(".search input");
@@ -55,3 +75,36 @@ async function checkWeather(city) {
  searchBtn.addEventListener("click",()=>{
 checkWeather(searchBox.value);
 })
+async function checkWeather1(city) {
+  const response= await fetch(apiurl + city + `&appid=${apikey}`);
+  var data = await response.json();
+  console.log(data);
+
+  document.querySelector(".bh1").innerHTML=Math.round(data.main.temp)+"°c";
+  document.querySelector(".w1").innerHTML = data.wind.speed ;
+  document.querySelector(".h1").innerHTML= data.main.humidity;
+
+}
+checkWeather1("bhopal");
+async function checkWeather2(city) {
+  const response= await fetch(apiurl + city + `&appid=${apikey}`);
+  var data = await response.json();
+  console.log(data);
+
+  document.querySelector(".bh2").innerHTML=Math.round(data.main.temp)+"°c";
+  document.querySelector(".w2").innerHTML = data.wind.speed ;
+  document.querySelector(".h2").innerHTML= data.main.humidity;
+
+}
+checkWeather2("mumbai");
+async function checkWeather3(city) {
+  const response= await fetch(apiurl + city + `&appid=${apikey}`);
+  var data = await response.json();
+  console.log(data);
+
+  document.querySelector(".bh3").innerHTML=Math.round(data.main.temp)+"°c";
+  document.querySelector(".w3").innerHTML = data.wind.speed ;
+  document.querySelector(".h3").innerHTML= data.main.humidity;
+
+}
+checkWeather3("delhi");
